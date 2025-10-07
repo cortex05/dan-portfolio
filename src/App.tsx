3f6b3facc22ab468006f230633f82bc3e2d219a9
cortex05 <havinginitiative@gmail.com>
2025-10-07 10:53:25 -0500

    feat: Basic landing set up

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useState, useEffect, useContext } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './styles/theme.css'
 import "./App.css"
-import Landing from "./screens/Landing"
+import Landing from "./screens/Landing/Landing"
 import Header from "./components/Header/Header"
 import { ThemeContext } from "./context/ThemeContext"
 import type { Theme } from "./types/constants"
+import Skills from "./screens/Skills/Skills"
+import Projects from "./screens/Projects/Projects"
+import Contact from "./screens/Contact/Contact"
 
 export const useTheme = () => {
   const context = useContext(ThemeContext)
@@ -33,6 +36,9 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </BrowserRouter>
       </ThemeContext.Provider>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,28 @@
-import { AppBar, Box, Toolbar } from '@mui/material'
-import React from 'react'
-import { ThemeToggle } from '../ThemeToggle/ThemeToggle'
+import { AppBar, Box, Toolbar } from "@mui/material";
+import React from "react";
+import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
+import styles from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate()
   return (
-    <Box sx={{ flexGrow: 1}}>
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          This is the header
+          <div className={styles.headerBody}>
+            <div className={styles.navLinks}>
+              <button onClick={() => navigate("/")}>About</button>
+              <button onClick={() => navigate("/skills")}>Skills</button>
+              <button onClick={() => navigate("/projects")}>Projects</button>
+              <button onClick={() => navigate("/contact")}>Contact</button>
+            </div>
+            <ThemeToggle />
+          </div>
         </Toolbar>
-        <ThemeToggle />
       </AppBar>
     </Box>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
