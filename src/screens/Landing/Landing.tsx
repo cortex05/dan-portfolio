f9b44b8de353cb705803bbbcc0e6bfafc6f49ab9
cortex05 <havinginitiative@gmail.com>
2025-10-07 16:29:05 -0500

    feat: added base content for about page

--- src/screens/Landing/Landing.tsx
+++ src/screens/Landing/Landing.tsx
@@ -1,15 +1,85 @@
-import React from 'react'
-import styles from './Landing.module.css'
+import React from "react";
+import styles from "./Landing.module.css";
 
 const Landing = () => {
   return (
     <div className={styles.landingContainer}>
-      <h1>This is the main page</h1>
-      <p>The plan here is to explain myself, maybe have a nice picture in the background, but to give a brief overview of what I have done. The navbar should always be visible to display the links to the other features. Maybe have some timeline/stats on my career here.
-      </p>
-      <p>List your work career here, this will be for the jobs you had and what you did at each. Think of it as a long resume</p>
+      <h1>Daniel Cortes - Software Engineer</h1>
+      <div className={styles.intro}>
+        <div>THis is a big block of text</div>
+        <div>THis is an image</div>
+      </div>
+      <div className={styles.employmentHistory}>
+        <h2>Employment History</h2>
+        <div>
+          <div>
+            <h3>Hoyack LLC</h3>
+            <p>July 2025 - September 2025</p>
+          </div>
+          <p>THis was my time at Hoyack</p>
+          <span>Tech used</span>
+          <ul>
+            <li>- React</li>
+            <li>- Javascript</li>
+            <li>- TypeScript</li>
+            <li>- Node.js</li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <h3>Kroger</h3>
+            <p>December 2021 - March 2025</p>
+          </div>
+          <p>I worked here for years</p>
+          <span>Tech used</span>
+          <ul>
+            <li>- React</li>
+            <li>- Javascript</li>
+            <li>- TypeScript</li>
+            <li>- Node.js</li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <h3>Ceballos Technologies</h3>
+            <p>September 2021 - December 2021</p>
+          </div>
+          <p>THis was my time at Hoyack</p>
+          <span>Tech used</span>
+          <ul>
+            <li>- React</li>
+            <li>- Javascript</li>
+            <li>- TypeScript</li>
+            <li>- Node.js</li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <h3>Nucamp</h3>
+            <p>March 2020 - September 2022</p>
+          </div>
+          <p>THis was my time at Hoyack</p>
+          <span>Tech used</span>
+          <ul>
+            <li>- React</li>
+            <li>- Javascript</li>
+            <li>- TypeScript</li>
+            <li>- Node.js</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.education}>
+        <h2>Education</h2>
+        <div>
+          <div>
+            <h3>CodeUp</h3>
+            <p>December 2020 - May 2021</p>
+          </div>
+          <p>Explain CodeUp here.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
