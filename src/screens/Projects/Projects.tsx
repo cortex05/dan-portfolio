2f631423021fcbc82e6e8734f7488f874720d3db
cortex05 <havinginitiative@gmail.com>
2025-10-08 10:14:02 -0500

    feat: Added skills responsiveness

--- src/screens/Projects/Projects.tsx
+++ src/screens/Projects/Projects.tsx
@@ -4,7 +4,7 @@ import styles from './Projects.module.css'
 const Projects = () => {
   return (
     <div className={styles.projectsContainer}>
-      <h1>The projects page</h1>
+      <h1>The projects page!</h1>
       <p>THis is where I will list projects I have made.</p>
       <p>The first section will be for live deployed projects. In the explanation, list when it was made</p>
       <p>This will be for projects that are no longer running or are only in the code stage such as Github</p>
